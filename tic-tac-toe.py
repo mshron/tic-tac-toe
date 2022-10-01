09e6370e84adb98fdbe861c1e1af227d29ac2926
Max Shron <max@shron.net>
2022-10-01 17:12:53 -0400

    Make whole table of tic-tac-toe games

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,4 +1,5 @@
 #!/bin/python
+from collections import defaultdict
 
 class State:
     def __init__(self, s: str = ""):
@@ -31,17 +32,27 @@ class State:
         ...
         ValueError: too many moves
 
+        >>> State("X").next
+        'O'
+
+        >>> State().next
+        'X'
+
         """
         if len(s) > 9:
             raise ValueError("too many moves")
         if set(s) - {'X', 'O', '_'} != set():
             raise ValueError("invalid characters")
-        move_differences = s.count("X") - s.count("O")
+        x_moves = s.count('X')
+        o_moves = s.count('O')
+        move_differences = x_moves - o_moves
         if move_differences > 1 or move_differences < 0:
             raise ValueError("illegal board state")
         self.s = dict(enumerate(s.ljust(9,"_")))
+        self.str = State.to_str(self.s)
         self.board = self.to_board()
-        self.victory = False
+        self.victory = self.check_status()
+        self.next = 'X' if x_moves == o_moves else 'O'
         self.leads_to_win = 0
         self.leads_to_tie = 0
         self.leads_to_loss = 0
@@ -54,23 +65,80 @@ class State:
            out[j][i] = self.s[v]
         return out
 
+    @staticmethod
+    def to_str(d: dict):
+        """
+        >>> State.to_str(State("X_O").s)
+        'X_O______'
+        """
+        return "".join([d[i] for i in range(9)])
+
+    def children(self):
+        """
+        >>> for child in State("X").children(): print(child)
+        XO_
+        ___
+        ___
+        <BLANKLINE>
+        X_O
+        ___
+        ___
+        <BLANKLINE>
+        X__
+        O__
+        ___
+        <BLANKLINE>
+        X__
+        _O_
+        ___
+        <BLANKLINE>
+        X__
+        __O
+        ___
+        <BLANKLINE>
+        X__
+        ___
+        O__
+        <BLANKLINE>
+        X__
+        ___
+        _O_
+        <BLANKLINE>
+        X__
+        ___
+        __O
+        <BLANKLINE>
+
+        >>> for child in State("XXXOO_O").children(): print(child)
+        """
+        if self.victory != "play":
+            return()
+
+        for i in range(9):
+            if self.s[i] == '_':
+                ss = self.s.copy()
+                ss[i] = self.next
+                yield State(State.to_str(ss))
+
     def __str__(self):
         """
         >>> print(State())
         ___
         ___
         ___
-
+        <BLANKLINE>
         >>> print(State("__X"))
         __X
         ___
         ___
-
+        <BLANKLINE>
         >>> print(State("_OX_OXO_X"))
         _OX
         _OX
         O_X
+        <BLANKLINE>
         """
+
         return("\n".join(["".join(row) for row in self.board]) + "\n")
 
     @staticmethod
@@ -146,6 +214,10 @@ class State:
         Loss diagnoal
         >>> State("X_OXO_OX").check_status()
         'loss'
+
+        Tie
+        >>> State("XOXXXOOXO").check_status()
+        'tie'
         """
         for r in self.rotations():
             if r[0] == ['X','X','X']:
@@ -156,22 +228,47 @@ class State:
                 return "win"
             elif r[0][0] == "O" and r[1][1] == "O" and r[2][2] == "O":
                 return "loss"
-        return "play"
+        if self.str.count('_') == 0:
+            return 'tie'
+        else:
+            return "play"
 
 
-def print_examples():
-   print(State((0,1,2)))
+def make_tree():
+    nodes = {"": State()}
+    children = defaultdict(list)
+    leaves = set()
 
-   print(State((0,1,3,4,6)))
+    visited = set()
+    to_visit = nodes.keys() - visited
+    while len(to_visit) > 0:
+        node_key = to_visit.pop()
+        node = nodes[node_key]
+        visited.add(node_key)
+        i = 0
+        for child in node.children():
+            s = child.str
+            children[node_key].append(s)
+            nodes[s] = child
+            i+=1
+        if i == 0: # no children; win/loss/tie node
+            leaves.add(node_key)
+        to_visit = nodes.keys() - visited
 
-   print(State((0,6,2,7,5,8)))
-
-   print(State((0,2,4,5,8)))
-
-   print(State((0,2,3,4,7,6)))
+    return({"nodes": nodes,
+            "children": children,
+            "leaves": leaves})
 
 def main():
-    print_examples()
+    res = make_tree()
+    for i, item in enumerate(res['children'].items()):
+        print(item)
+        if i == 10:
+            break
+    c = defaultdict(int)
+    for i, item in enumerate(res['leaves']):
+        c[res['nodes'][item].victory] += 1
+    print(c)
 
 if __name__ == "__main__":
     main()
